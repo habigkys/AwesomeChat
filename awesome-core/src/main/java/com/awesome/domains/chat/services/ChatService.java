bf8084cf4440697506ef44e1fcfcf61c48ddcbc7
LowMental <habigkys@naver.com>
2021-11-15 18:17:48 +0900

    정리

--- awesome-core/src/main/java/com/awesome/domains/chat/services/ChatService.java
+++ awesome-core/src/main/java/com/awesome/domains/chat/services/ChatService.java
@@ -3,21 +3,13 @@ package com.awesome.domains.chat.services;
 import com.awesome.domains.chat.dtos.ChatMessageDTO;
 import com.awesome.domains.chat.dtos.ChatRoomDTO;
 import com.awesome.domains.chat.entities.ChatMessageDAO;
-import com.awesome.domains.chat.entities.ChatMessageEntity;
 import com.awesome.domains.chat.entities.ChatRoomDAO;
 import com.awesome.domains.chat.entities.ChatRoomEntity;
 import com.awesome.domains.mapping.entities.ChatUserRoomDAO;
 import com.awesome.domains.mapping.entities.ChatUserRoomEntity;
-import com.awesome.domains.project.dtos.ProjectDTO;
-import com.awesome.domains.project.entities.ProjectEntity;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
